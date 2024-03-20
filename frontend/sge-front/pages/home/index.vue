0a8af562833a31764d4ab7b5388e2ac7c0016063
Desenvolvimento <dsadm@educ501.local>
2024-03-19 21:00:33 -0300

    sge_verel

---
+++ frontend/sge-front/pages/home/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { signOut } = useAuth();
+definePageMeta({
+    middleware: 'auth'
+})
+
+const submitLogout = ()=>{
+
+}
+</script>
+
+<template>
+    <div>
+        <h1>Minha Tela Home!!</h1>
+        <button @click="submitLogout" class="customButton">SAIR</button>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
